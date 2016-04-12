f527f071b2829aaf971f14214a6fd0b06f3d9595
Martin Moser <moser@emineo.ch>
2016-04-12 07:35:42 +0200

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var siClaro = angular.module('siClaro', ['ngRoute','infinite-scroll']);
+
+siClaro.config(function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'templates/imageList.html',
+        controller: 'ImageListCtrl'
+    })
+});
