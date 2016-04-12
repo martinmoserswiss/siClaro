ba557d4b2a5b5c2b08edf105ef365be0c738f58a
Martin Moser <moser@emineo.ch>
2016-04-12 23:50:56 +0200

    #1

--- scripts/controllers/imageListController.js
+++ scripts/controllers/imageListController.js
@@ -1,10 +1,14 @@
 siClaro.controller('imageListController', function($scope) {
-    $scope.images = ["portugal/p14-1-10.jpg", "baja/P1000394.jpg", "baja/P1000432.jpg", "baja/P1010144.jpg", "baja/P1010197.jpg", "baja/P1010287.jpg", "london/l1.jpg", "london/l2.jpg"];
-
+    $scope.imagesStock = ["portugal/p14-1-10.jpg", "baja/P1000394.jpg", "baja/P1000432.jpg", "baja/P1010144.jpg", "baja/P1010197.jpg", "baja/P1010287.jpg", "london/l1.jpg", "london/l2.jpg"];
+    $scope.images = [];
     $scope.loadMore = function() {
-        var last = $scope.images[$scope.images.length - 1];
-        for(var i = 1; i <= 3; i++) {
-            $scope.images.push(last + i);
+      var length = $scope.images.length;
+      for(var i = 0; i <= 2; i++) {
+        if($scope.imagesStock.length - 1 >= ($scope.images.length + i)) {
+          var element = $scope.imagesStock[length+i];
+          console.log(element);
+          $scope.images.push(element);
         }
+      }
     };
 });
